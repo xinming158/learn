4c8949a023bb2de4331ecea21632eb9145ceadb2
xinming158 <xinmingme@126.com>
2019-06-27 20:45:08 +0800

    初始化项目

---
+++ src/main/java/com/xinming/App.java
@@ -0,0 +1,13 @@
+package com.xinming;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
